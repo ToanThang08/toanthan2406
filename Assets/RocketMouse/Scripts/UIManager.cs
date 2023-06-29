769ba44bdf197381b4a1940b276c65b45716d3bc
Toan Thang <box.dota@gmail.com>
2023-06-29 21:22:17 +0300

    Intro Unity UI - Part1

--- Assets/RocketMouse/Scripts/UIManager.cs
+++ Assets/RocketMouse/Scripts/UIManager.cs
@@ -5,10 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    public Animator startButton;
+    public Animator settingsButton;
+
+    public Animator dialog;
     // Start is called before the first frame update
     public void StartGame()
     {
         SceneManager.LoadScene("RocketMouse");
+    }
+
+    public void OpenSettings()
+    {
+        startButton.SetBool("isHidden", true);
+        settingsButton.SetBool("isHidden", true);
+        //unity-ui-part2
+        dialog.SetBool("isHidden", false);
+    }
+
+    public void CloseSettings()
+    {
+        startButton.SetBool("isHidden", false);
+        settingsButton.SetBool("isHidden", false);
+        dialog.SetBool("isHidden", true);
     }
 
 }
